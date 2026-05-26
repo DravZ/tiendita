f48d418d1f3a6d08b6091d4f7a3cc9cbfc14a044
skarmik <armandomca2111@agmail.com>
2026-05-26 07:52:24 -0600

    Table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Products } from './pages/products/products';
 
 export const routes: Routes = [
   {
     path: 'dashboard',
     component: Dashboard
+  },
+  {
+    path: 'products',
+    component: Products
   },
   {
     path: '**',
