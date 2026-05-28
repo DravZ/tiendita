c9667932aba267c6947f2ee68e3ad1a22f674a92
skarmik <armandomca2111@agmail.com>
2026-05-28 08:43:09 -0600

    fix: products/clients

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './products.html',
   styles: `
     :host {
